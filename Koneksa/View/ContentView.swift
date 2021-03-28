591eff166aaa72b1a19cfe0a483c37933b2c7864
Nick Nguyen <nicknguyenios14@gmail.com>
2021-03-28 11:14:55 -0400

    Change the text font so the text won't shake when timer run.

--- Koneksa/View/ContentView.swift
+++ Koneksa/View/ContentView.swift
@@ -10,63 +10,73 @@ import CoreMotion
 
 struct ContentView: View {
 
-    let motion = CMMotionManager()
-    let queue = OperationQueue()
-    let recorder = CMSensorRecorder()
+    // MARK: - Properties
+
+    private let motion = CMMotionManager()
 
     @State private var x: Double = 0
     @State private var y: Double = 0
     @State private var z: Double = 0
     @State private var isVisible: Bool = false
 
+    // MARK: - View
 
     var body: some View {
 
         VStack {
-            Text("X: \(x),Y: \(y),Z: \(z)")
-                .opacity(!isVisible ? 0 : 1)
+            VStack {
+                Text("X:\(x)")
+                Text("Y:\(y)")
+                Text("Z:\(z)")
+            }
+            .opacity(!isVisible ? 0 : 1)
+            .font(.custom("Menlo-Regular", size: 19))
 
             Divider()
 
             Button("Start") {
-                print("GET accelerometer data for 10 seconds")
-                // After 10 seconds, write to disk
                 startAccelerometers()
             }
             .opacity(isVisible ? 0 : 1)
         }
     }
 
-    func startAccelerometers() {
-        // Make sure the accelerometer hardware is available.
-        isVisible.toggle()
-        var count: Double = 0
+    // MARK: - Action
 
+    func startAccelerometers() {
+
+        isVisible.toggle()
+        var cycles: Double = 0
+        // Make sure the accelerometer hardware is available.
         if motion.isAccelerometerAvailable {
             motion.accelerometerUpdateInterval = 1.0 / 50.0  // 50 Hz
             motion.startAccelerometerUpdates()
 
             // Configure a timer to fetch the data.
 
-            let timer = Timer(fire: Date(),
-                              interval: (1.0 / 50.0),
-                              repeats: true,
-                              block: { timer in
-                                // Get the accelerometer data.
-                                count += 1
-                                print(count)
-                                if let data = motion.accelerometerData {
-                                    // Use the accelerometer data in your app.
-                                    x = data.acceleration.x
-                                    y = data.acceleration.y
-                                    z = data.acceleration.z
-                                }
-                                if count == 500.0 {
-                                    motion.stopAccelerometerUpdates()
-                                    timer.invalidate()
-                                    isVisible.toggle()
-                                }
-                              })
+            let timer = Timer(
+                fire: Date(),
+                interval: (1.0 / 50.0),
+                repeats: true,
+                block: { timer in
+                    cycles += 1
+
+                    // Get the accelerometer data.
+                    if let data = motion.accelerometerData {
+
+                        x = data.acceleration.x
+                        y = data.acceleration.y
+                        z = data.acceleration.z
+                        // the timer run 50 cycles per second, 10 seconds will equal 500 cycles.
+                        if cycles == 500.0 {
+                            timer.invalidate()
+                            motion.stopAccelerometerUpdates()
+                            isVisible.toggle()
+                            // TODO: Write data to disk.
+                        }
+                    }
+                }
+            )
 
             // Add the timer to the current run loop.
             RunLoop.current.add(timer, forMode: .default)
@@ -79,9 +89,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
-extension CMSensorDataList: Sequence {
-    public typealias Iterator = NSFastEnumerationIterator
-    public func makeIterator() -> NSFastEnumerationIterator {
-        return NSFastEnumerationIterator(self)
-    }
-}
+
