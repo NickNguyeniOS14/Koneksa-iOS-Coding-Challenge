9c31c7086a310d8c53f04d122fed5e3539edafa5
Nick Nguyen <nicknguyenios14@gmail.com>
2021-03-28 09:01:14 -0400

    Initial Commit

---
+++ Koneksa/KoneksaApp.swift
@@ -0,0 +1,17 @@
+//
+//  KoneksaApp.swift
+//  Koneksa
+//
+//  Created by Nick Nguyen on 3/28/21.
+//
+
+import SwiftUI
+
+@main
+struct KoneksaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
