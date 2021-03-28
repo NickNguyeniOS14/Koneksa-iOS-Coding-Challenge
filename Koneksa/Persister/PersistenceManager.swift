55cafbf356a7f26fa7b4b5b405685703301611c7
Nick Nguyen <nicknguyenios14@gmail.com>
2021-03-28 13:09:20 -0400

    Write data to disk.

---
+++ Koneksa/Persister/PersistenceManager.swift
@@ -0,0 +1,40 @@
+//
+//  PersistenceManager.swift
+//  Koneksa
+//
+//  Created by Nick Nguyen on 3/28/21.
+//
+
+import Foundation
+
+struct Persister {
+
+    let fileURL: URL
+
+    let plistEncoder = PropertyListEncoder()
+
+    let plistDecoder = PropertyListDecoder()
+
+    init?(withFileName fileName: String) {
+        guard let url = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent(fileName) else {
+            return nil
+        }
+        print("PATH: \(url)")
+        fileURL = url
+    }
+
+    func save<T: Codable>(_ object: T) {
+        do {
+            let accelerometerData = try plistEncoder.encode(object)
+            try accelerometerData.write(to: fileURL)
+        } catch let err as NSError {
+            print(err.localizedDescription)
+        }
+    }
+
+    func fetch<T: Codable>() throws -> T {
+        let accelerometerData = try Data(contentsOf: fileURL)
+        let data = try plistDecoder.decode(T.self, from: accelerometerData)
+        return data
+    }
+}
--- Koneksa/View/ContentView.swift
+++ Koneksa/View/ContentView.swift
@@ -14,6 +14,8 @@ struct ContentView: View {
 
     private let motion = CMMotionManager()
 
+    private let persister = Persister(withFileName: "AccelerometerData")
+
     @State private var x: Double = 0
     @State private var y: Double = 0
     @State private var z: Double = 0
@@ -39,15 +41,30 @@ struct ContentView: View {
             }
             .opacity(isVisible ? 0 : 1)
         }
+        .onAppear {
+            /* Check to see if data is stored to disk properly by printing it out on console after app's launch
+             or use this reference: https://stackoverflow.com/questions/38064042/access-files-in-var-mobile-containers-data-application-without-jailbreaking-iph/38064225
+             */
+            if let accelerometerData: [String] = try? persister?.fetch() {
+                accelerometerData.forEach { (data) in
+                    print(data)
+                }
+            }
+        }
     }
 
     // MARK: - Action
 
     func startAccelerometers() {
 
+        var accelerometerData = [String]()
+
         isVisible.toggle()
-        var cycles: Double = 0
+        
+        var cycles: Double = 0.0
+
         // Make sure the accelerometer hardware is available.
+
         if motion.isAccelerometerAvailable {
             motion.accelerometerUpdateInterval = 1.0 / 50.0  // 50 Hz
             motion.startAccelerometerUpdates()
@@ -60,24 +77,29 @@ struct ContentView: View {
                 repeats: true,
                 block: { timer in
                     cycles += 1
-
                     // Get the accelerometer data.
                     if let data = motion.accelerometerData {
 
                         x = data.acceleration.x
                         y = data.acceleration.y
                         z = data.acceleration.z
-                        // the timer run 50 cycles per second, 10 seconds will equal 500 cycles.
+
+                        let stringData = "X: \(x),Y: \(y),Z:\(z)"
+
+                        accelerometerData.append(stringData)
+
+                        /* The timer runs 50 cycles per second, 10 seconds will equal 500 cycles,
+                         that's when we need to stop and write data to disk. */
+
                         if cycles == 500.0 {
                             timer.invalidate()
                             motion.stopAccelerometerUpdates()
                             isVisible.toggle()
-                            // TODO: Write data to disk.
+                            persister?.save(accelerometerData)
                         }
                     }
                 }
             )
-
             // Add the timer to the current run loop.
             RunLoop.current.add(timer, forMode: .default)
         }
@@ -89,4 +111,3 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
-
